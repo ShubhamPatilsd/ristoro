94cf00212bb3f58e8563064ee6cda808bab0dedd
Shubham Patil <shubhampatilsd@gmail.com>
2024-08-04 14:12:34 -0700

    push

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
     extend: {
       backgroundImage: {
@@ -16,5 +17,6 @@ const config: Config = {
     },
   },
   plugins: [],
+  darkMode: "selector",
 };
 export default config;
