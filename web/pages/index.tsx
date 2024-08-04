7dbdd7612a27897c0bda0c9d7cf9b21f2c2544c3
Shubham Patil <shubhampatilsd@gmail.com>
2024-08-04 14:00:20 -0700

    push

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -30,22 +30,19 @@ export default function Home() {
     <div className="bg-[url('/sky.jpg')] h-screen w-screen flex justify-center items-center flex-col space-y-2">
       <Head>
         <title>Ristoro</title>
-        <meta
-          name="description"
-          content="Get expert restaurant recommendations tailored just for you with Ristoro!"
-        />
+        <meta name="description" content="Text EAT to 510-579-7965" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-3xl md:text-[10vw] mb-2 font-bold text-center">
+      <h1 className="text-3xl text-black md:text-[10vw] mb-2 font-bold text-center">
         hungry?
       </h1>
-      <h1 className="text-2xl  md:text-[5vw] font-bold text-center">
+      <h1 className="text-2xl text-black md:text-[5vw] font-bold text-center">
         text "EAT" to{" "}
-        <a href="sms:+15105797965&body=EAT" className="underline">
+        <a href="sms://+15105797965;?&body=EAT" className="underline">
           510-579-7965
         </a>{" "}
       </h1>
-      <h1 className="text-xl md:text-[3vw] font-bold text-center">
+      <h1 className="text-xl text-black md:text-[3vw] font-bold text-center">
         for a <span className="italic">really</span> good food rec
       </h1>
     </div>
