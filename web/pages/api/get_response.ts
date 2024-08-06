b9d9b2cac144a079c84297c60856b9a98e6ea02e
Shubham Patil <shubhampatilsd@gmail.com>
2024-08-05 17:58:40 -0700

    groq

--- web/pages/api/get_response.ts
+++ web/pages/api/get_response.ts
@@ -69,6 +69,7 @@ export default async function handler(
     inputVariables: ["question"],
   });
 
+  //@ts-ignore
   const chain = promptTemplate.pipe(model);
 
   const response = await chain.invoke({
