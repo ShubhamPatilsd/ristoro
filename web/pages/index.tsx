308b8471136d8f39673ebbfe0af77d13bc90afd0
Shubham Patil <shubhampatilsd@gmail.com>
2024-08-08 00:24:46 -0700

    ui changes

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -169,144 +169,11 @@ export default function Home() {
   };
 
   return (
-    <main>
-      <div style={{ height: "100vh", width: "100%" }}>
-        <Map
-          ref={map}
-          mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-          style={{ width: "100vw", height: "100vh" }}
-          initialViewState={{
-            // longitude: -122.442982,
-            // latitude: 37.768649,
+    <main className="h-screen">
+      <div className="bg-sky-50 border border-gray-300 shadow-xl flex justify-between items-center px-8">
+        <img src="/smalllogo.svg" className="h-14" />
 
-            longitude: -122.412784,
-            latitude: 37.765217,
-            zoom: 12,
-          }}
-          maxBounds={[
-            [-122.690114, 37.632134],
-            [-122.19297, 37.876975],
-          ]}
-          mapStyle="mapbox://styles/mapbox/streets-v9"
-        >
-          <GeolocateControl
-            ref={geolocateControlRef}
-            trackUserLocation
-            positionOptions={{ enableHighAccuracy: true }}
-          />
-
-          {content && (
-            <>
-              <Marker
-                onClick={() => {
-                  const p = point([
-                    parseFloat(content.location.longitude),
-                    parseFloat(content.location.latitude),
-                  ]);
-                  const bufferStat = buffer(p, 0.5, { units: "kilometers" });
-                  const [minLng, minLat, maxLng, maxLat] = bbox(bufferStat);
-
-                  map.current?.fitBounds(
-                    [
-                      [minLng, minLat],
-                      [maxLng, maxLat],
-                    ],
-                    { padding: 40, duration: 1000 }
-                  );
-                }}
-                longitude={content.location.longitude}
-                latitude={content.location.latitude}
-                color="#fff"
-                // popup={popup}
-                ref={markerRef}
-              >
-                <div className="marker" />
-              </Marker>
-              {/* <Popup
-                longitude={content.location.longitude}
-                closeOnClick={false}
-                closeOnMove={false}
-                onClose={() => {}}
-                latitude={content.location.latitude}
-                anchor="top"
-              >
-                <div className="p-4 bg-white rounded-sm">
-                  <h1>{content.name}</h1>
-                  <p>{content.address}</p>
-                  <p>{content.description}</p>
-                </div>
-              </Popup> */}
-
-              {content && (
-                <div className="shadow-2xl md:w-[50vw] overflow-y-scroll fixed bg-gradient-to-r from-stone-50 to-stone-100 bottom-0 md:left-12 bg-white p-8 h-[50vh] md:h-[70vh]">
-                  <p className="font-medium text-xl">
-                    <span className="text-gray-600 font-medium">
-                      {`${findTimeOfDay()}${user ? ` ${user?.firstName}` : ""}`}
-                      ,
-                    </span>
-                  </p>
-
-                  <div className="flex h-72 mt-2">
-                    <div className="w-full overflow-x-scroll max-w-[80vw] space-x-3 flex">
-                      {content.information.photos.map((photo) => {
-                        return (
-                          <div
-                            className="h-full min-w-[60vw]  md:min-w-[20vw] rounded-md bg-cover"
-                            style={{
-                              backgroundImage: `url('${photo.prefix}original${photo.suffix}')`,
-                            }}
-                          />
-                        );
-                      })}
-                    </div>
-                  </div>
-
-                  <div
-                    className="text-lg mt-3 letterParent"
-                    dangerouslySetInnerHTML={{ __html: content.letter }}
-                  />
-
-                  {/* <div className="text-lg mt-3 letterParent">
-                    {animatedLetterContent.map((sentence, index) => (
-                      <span
-                        key={index}
-                        className="fade-in-sentence"
-                        style={{ animationDelay: `${index * 0.5}s` }}
-                      >
-                        {sentence}{" "}
-                      </span>
-                    ))}
-                  </div> */}
-
-                  {/* <div
-                    className="text-lg mt-3 letterParent"
-                    dangerouslySetInnerHTML={{ __html: animatedLetterContent }}
-                  /> */}
-
-                  {/* <button
-                  onClick={() => {
-                    fetch("/api/mark_visited", {
-                      method: "POST",
-                      body: JSON.stringify({
-                        restaurantName: content.name,
-                      }),
-                    })
-                      .then((res) => res.json())
-                      .then((data) => {
-                        setContent("");
-                        setLetter("");
-                      });
-                  }}
-                >
-                  Visited!
-                </button> */}
-                </div>
-              )}
-            </>
-          )}
-        </Map>
-
-        <div className="absolute top-10 z-0 left-0 flex items-center justify-center w-full">
+        <div className="">
           <div className="p-4">
             <form
               onSubmit={async (e) => {
@@ -514,20 +381,20 @@ export default function Home() {
                   break;
                 }
               }}
-              className="flex items-center group bg-[url(/sky.jpg)] bg-cover rounded-full bg-blend-overlay"
+              className="flex items-center group bg-white bg-cover rounded-full bg-blend-overlay border border-sky-500"
             >
               <input
                 type="text"
                 name="query"
-                placeholder="Let's try something new today"
-                className=" font-semibold text-invert focus:outline-none placeholder:invert placeholder:opacity-55  bg-transparent rounded-full rounded-r-none  px-6 py-2 border-2 border-sky-600 border-r-0   md:w-[35vw] md:text-2xl"
+                placeholder="I want to eat some..."
+                className=" font-semibold  focus:outline-none  placeholder:opacity-80  bg-transparent rounded-full rounded-r-none  px-6 py-2 border-0 border-sky-600 border-r-0   md:w-[35vw] md:text-2xl"
                 onChange={(e) => {
                   setQuery(e.target.value);
                 }}
               />
               <button
                 type="submit"
-                className="focus:outline-none py-3 text-2xl focus:ring text-sky-500 font-bold focus:ring-sky-300 bg-transparent rounded-full rounded-l-none border-2 border-l-1 border-l-[1px] px-6 border-sky-600 ring-sky-200"
+                className="focus:outline-none py-3 text-2xl focus:ring text-sky-500 font-bold focus:ring-sky-300 bg-transparent rounded-full rounded-l-none border-0 px-6 border-sky-600 ring-sky-200"
               >
                 <IoSearchOutline className="mx-auto" />
               </button>
@@ -536,19 +403,10 @@ export default function Home() {
           {/* <div dangerouslySetInnerHTML={{ __html: content }} /> */}
           <div />
         </div>
-        <div className="absolute top-5 left-5 z-99">
+        <div className=" z-99">
           {user ? (
             <UserButton />
           ) : (
-            // <button
-            //   onClick={() => {
-
-            //   }}
-            //   className="rounded-md bg-sky-500 border-2 border-sky-400 shadow px-3 py-1 font-bold text-white text-lg"
-            // >
-            //   Log In
-            // </button>
-
             <SignInButton
               //@ts-ignore
               className={
@@ -557,6 +415,92 @@ export default function Home() {
             />
           )}
         </div>
+      </div>
+      <div
+        style={{ height: "100%", width: "100%" }}
+        className="flex flex-col md:flex-row"
+      >
+        {content && (
+          <div className="md:w-[50vw] overflow-y-scroll bg-gradient-to-r from-stone-50 to-stone-100 bottom-0 md:left-12 bg-white p-8 h-[50vh] md:h-[100vh]">
+            <p className="font-medium text-xl">
+              <span className="text-gray-600 font-medium">
+                {`${findTimeOfDay()}${user ? ` ${user?.firstName}` : ""}`},
+              </span>
+            </p>
+
+            <div className="flex h-72 mt-2">
+              <div className="w-full overflow-x-scroll max-w-[80vw] space-x-3 flex">
+                {content.information.photos.map((photo) => {
+                  return (
+                    <div
+                      className="h-full min-w-[60vw]  md:min-w-[20vw] rounded-md bg-cover"
+                      style={{
+                        backgroundImage: `url('${photo.prefix}original${photo.suffix}')`,
+                      }}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+
+            <div
+              className="text-lg mt-3 letterParent"
+              dangerouslySetInnerHTML={{ __html: content.letter }}
+            />
+          </div>
+        )}
+
+        <Map
+          ref={map}
+          mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+          style={{ width: "100vw", height: "100%" }}
+          initialViewState={{
+            longitude: -122.412784,
+            latitude: 37.765217,
+            zoom: 12,
+          }}
+          maxBounds={[
+            [-122.690114, 37.632134],
+            [-122.19297, 37.876975],
+          ]}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+        >
+          <GeolocateControl
+            ref={geolocateControlRef}
+            trackUserLocation
+            positionOptions={{ enableHighAccuracy: true }}
+          />
+
+          {content && (
+            <>
+              <Marker
+                onClick={() => {
+                  const p = point([
+                    parseFloat(content.location.longitude),
+                    parseFloat(content.location.latitude),
+                  ]);
+                  const bufferStat = buffer(p, 0.5, { units: "kilometers" });
+                  const [minLng, minLat, maxLng, maxLat] = bbox(bufferStat);
+
+                  map.current?.fitBounds(
+                    [
+                      [minLng, minLat],
+                      [maxLng, maxLat],
+                    ],
+                    { padding: 40, duration: 1000 }
+                  );
+                }}
+                longitude={content.location.longitude}
+                latitude={content.location.latitude}
+                color="#fff"
+                // popup={popup}
+                ref={markerRef}
+              >
+                <div className="marker" />
+              </Marker>
+            </>
+          )}
+        </Map>
       </div>
     </main>
   );
