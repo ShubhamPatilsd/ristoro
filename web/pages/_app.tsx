7bde240c3afd16cf986021a3472bc49b086f80b0
Shubham Patil <shubhampatilsd@gmail.com>
2024-08-20 09:16:39 -0700

    seo

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -9,7 +9,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SignedOutPage } from "@/components/SignedOutPage";
 import { Analytics } from "@vercel/analytics/react";
-import { Head } from "next/document";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
